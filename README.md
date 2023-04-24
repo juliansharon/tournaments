9c665bd0747352dafe63387f46912b826b319743
julian <julianshrn@gmail.com>
2023-04-24 22:29:12 +0530

    added leaderboard serverless function

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # YET TO DONE
 
-Maintain lobby details in a seperate Table
-Add stream for dynamo db and update tournament status based
-Add web socket of realtime updates of data to the frontend
+* Maintain lobby details in a seperate Table
+* Add stream for dynamo db and update tournament status based on the lobby
+* Add web socket of realtime updates of data to the frontend
+
--- getLeaderBoard/app.js
+++ getLeaderBoard/app.js
@@ -4,41 +4,21 @@ const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: 
 
 exports.handler = async event => {
 
-    const userId = event.body.userId;
-    const tournamentId = event.body.tournamentId;
-    const option = event.body.option;
-    console.log(process.env.TABLE_NAME,)
-    const random = Math.random();
-    const addScore = 0;
-    var genOption;
-    if (random <= 0.5) {
-        genOption = 0;
-    }
-    else {
-        genOption = 1;
-    }
-    if (genOption === option) {
-        addScore += 5;
-    }
-    const addparams = {
-        TableName: process.env.TABLE_NAME,
-        Key: {
-            tournamentId: tournamentId,
-        },
-        UpdateExpression: "SET score = if_not_exists(score, :initial) + :num,userId = :userId, remaining =if_not_exists(remaining, :initialRemaining) - :num",
+
+    const tournamentId = JSON.parse(event.body).tournamentId;
+
+    const queryParms = {
+        TableName: process.env.SCORE_TABLE_NAME,
+        KeyConditionExpression: 'tournamentId = :tournamentId',
         ExpressionAttributeValues: {
-            ":num": 1,
-            ":initial": 0,
-            ":userId": userId,
-            ":initialRemaining": 5,
-        },
+            ':tournamentId': tournamentId,
+        }
     };
-
-
     try {
-        await ddb.update(addparams).promise()
+        const leaderboard = await ddb.query(queryParms).promise()
+        return { statusCode: 200, body: JSON.stringify({ leaderboard }) };
     } catch (err) {
-        return { statusCode: 500, body: 'Failed to add: ' + JSON.stringify(err) };
+        return { statusCode: 500, body: JSON.stringify(err) };
     }
-    return { statusCode: 200, body: JSON.stringify('created tournament') };
+
 };
